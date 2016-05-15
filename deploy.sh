bcdf525987ca7263cde9dc2bd01034ebe16adc65
Jason.Zhang <lefttjs@gmail.com>
2016-05-15 10:29:41 +0800

    fix project name

--- deploy.sh
+++ deploy.sh
@@ -3,5 +3,5 @@ git reset --hard
 git pull origin HEAD
 npm install
 npm run dist
-pm2 stop uphall -f
-pm2 start ./dist/index.js --node-args="--env=dist" -n uphall
+pm2 stop uphall-web -f
+pm2 start ./dist/index.js --node-args="--env=dist" -n uphall-web
